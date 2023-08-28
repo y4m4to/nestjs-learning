1d508cec666ff03733298580b5d3719b1ee39c1a
y4m4to <ya.ponpome@gmail.com>
2023-08-28 23:38:41 +0900

    TypeORMのセットアップ

---
+++ typeOrm.config.ts
@@ -0,0 +1,23 @@
+import { DataSource } from 'typeorm';
+
+export default new DataSource({
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  database: 'test',
+  username: 'test',
+  password: 'password',
+  entities: ['dist/**/entities/**/*.entity.js'],
+  migrations: ['dist/**/migrations/**/*.js'],
+
+  // ログを出力するかどうか
+  logging: true,
+
+  /**
+   * @note
+   * synchronize は開発時にのみ使用する。
+   * trueにすると、エンティティの変更を検知して、自動的にテーブルが更新される
+   * @todo 本番環境では、falseにすること
+   **/
+  synchronize: true,
+});
