f65041feb6066d47a1022fd78f758ffd5ba7f770
y4m4to <ya.ponpome@gmail.com>
2023-09-20 01:19:45 +0900

    JWT Tokenの返却を実装する

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,13 @@
 import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { UsersService } from "src/users/users.service";
+import { JwtService } from "@nestjs/jwt";
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
   async singIn(username: string, password: string): Promise<any> {
     const user = await this.usersService.findOneByName(username);
@@ -11,7 +15,9 @@ export class AuthService {
     if (user?.passwordHash !== password) {
       throw new UnauthorizedException();
     }
-    const { ...result } = user;
-    return result;
+    const payload = { sub: user.id, username: user.name };
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
   }
 }
---
+++ src/auth/constants.ts
@@ -0,0 +1,5 @@
+// FIXME: このコードは公開しない。環境変数で管理する。
+export const jwtConstants = {
+  secret:
+    "DO NOT USE THIS VALUE. INSTEAD, CREATE A COMPLEX SECRET AND KEEP IT SAFE OUTSIDE OF THE SOURCE CODE.",
+};
